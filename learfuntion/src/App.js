0fc64d59250584248a04bb287ea323b79293b5e2
MrCarlos <caborregos@unimagdalena.edu.co>
2022-04-18 21:57:13 -0500

    Creamos un contador de clicks con useState "customizado"

--- learfuntion/src/App.js
+++ learfuntion/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const useContador =(inicial)=>{
+  const [contador, setConatdor] = useState(inicial)
+  const incrementar =()=>{
+    setConatdor(contador+1)
+  }
+  return[contador, incrementar]
+}
+
+let App =()=>{
+  const [contador, incrementar] = useContador(0)
+  return(
+    <div>
+      <h2>Contador:<h1>{contador}</h1></h2>
+      <button onClick={incrementar}>Incrementar</button>
     </div>
   );
 }
-
 export default App;
